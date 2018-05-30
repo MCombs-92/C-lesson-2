f1815c1f3f7af3d543dae9a61d1505b70ccdff63
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-30 15:15:46 -0400

    Initial commit for Csharp lesson 2

--- CSharpLesson2/Program.cs
+++ CSharpLesson2/Program.cs
@@ -8,8 +8,27 @@ namespace CSharpLesson2
 {
 	class Program
 	{
+
+		static int Add(int nbr, int inc)
+		{
+			return nbr + inc;
+		}
+
+		static int Add1(int nbr)
+		{
+			return Add(nbr,1);
+		}
 		static void Main(string[] args)
 		{
+			int nbrs = 12;
+			int result = Add1(nbrs);
+			Console.WriteLine("{0} Add1 is {1}", nbrs, result);
+			result = Add(nbrs, -2);
+			Console.WriteLine("{0} Add is {1}", nbrs, result);
 		}
+
+		
 	}
+
+
 }
